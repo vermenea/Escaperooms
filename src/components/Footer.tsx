7f5fbb0e5d16d5e912cff9b8711906d42f010ca6
Lukasz Lesniewski <lukasz.revol@gmail.com>
2024-11-15 00:23:25 +0100

    dodano componenty Header,Main,Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default function Main() {
+	return (
+		<footer>
+			<h1 className='footer-title'>naglowek stopki</h1>
+		</footer>
+	);
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
-import Faq from '../pages/Faq';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
   },
-  {
-    path: '/Faq',
-    element: <Faq />
-  }
 ]);
